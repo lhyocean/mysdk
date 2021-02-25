3caeb4d2ba6f14feaee77fc542e87907ba5ad595
lihaiyang <lihaiyang@bjebc.com>
2021-02-25 10:09:04 +0800

    first

---
+++ mysdk/src/main/java/com/bj/go/mysdk/TestUtil.java
@@ -0,0 +1,15 @@
+package com.bj.go.mysdk;
+
+import android.util.Log;
+
+/**
+ * Created by ocean on 2021-02-25
+ *
+ * @describe:
+ */
+public class TestUtil {
+
+    public  void testMethod(){
+        Log.e("TestUtil", "testMethod: " );
+    }
+}
